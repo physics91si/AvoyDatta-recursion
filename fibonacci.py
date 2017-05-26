7c9bef519ce180b243ab7977abb0f26cfc0b219b
Avoy Datta <avoy.dattal@stanford.edu>
2017-05-25 18:35:19 -0700

    Lab16 done

--- fibonacci.py
+++ fibonacci.py
@@ -1,4 +1,28 @@
 #!/usr/bin/env python3
-
+import sys
 # Lab 16: recursion
 # Fibonacci with cache
+
+
+sys.setrecursionlimit(5000000)
+
+global fib_cache
+fib_cache = {}
+
+def main():
+	print(fibonacci(0))
+	print(fibonacci(100))
+	print(fibonacci(101))
+
+def fibonacci(n):
+	if n == 0 or n == 1:
+		return n
+	elif n in fib_cache:
+		return fib_cache.get(n)
+	else:
+		x = fibonacci(n-1) + fibonacci(n-2)
+		fib_cache[n] = x
+		print("Recursion step")
+		return x
+
+main()
--- levenshtein.py
+++ levenshtein.py
@@ -2,3 +2,31 @@
 
 # Lab 16: Recursion
 # Levenshtein Distance
+
+global lev_cache
+lev_cache = {}
+
+def main():
+	str1 = "armhair"
+	str2 = "armchair"
+	print(lev(str1, str2))
+
+def lev(str1, str2):
+	if str1 == str2 :
+		return 0
+	if str1 == "" or str2 == "":
+		return max([len(str1), len(str2)])
+
+	else:
+		if str1[-1] == str2[-1]:
+			return lev(str1[:-1], str2[:-1])
+		else:
+			l1 = lev(str1,str2[:-1])
+			l2 = lev(str1[:-1], str2)
+			l3 = lev(str1[:-1], str2[:-1])
+
+			levVal = min([l1, l2, l3]) + 1
+			return levVal
+
+
+main()
